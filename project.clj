5230e3909c4c9be5f0ee87622fe84c91bf30b800
Ryan Sundberg <ryan.sundberg@gmail.com>
2018-06-23 13:21:49 -0700

    Use Hikari connection pool for concurrency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   [[org.clojure/clojure "1.8.0"]
    [org.clojure/core.async "0.4.474"]
    [arctype/service "0.1.0-SNAPSHOT"]
-   [org.xerial/sqlite-jdbc "3.23.1"]])
+   [org.xerial/sqlite-jdbc "3.23.1"]
+   [hikari-cp "2.5.0"]])
--- src/arctype/service/io/sqlite.clj
+++ src/arctype/service/io/sqlite.clj
@@ -4,6 +4,7 @@
     [clojure.core.async :as async]
     [clojure.java.jdbc :as jdbc]
     [clojure.tools.logging :as log]
+    [hikari-cp.core :as hikari-cp]
     [schema.core :as S]
     [sundbry.resource :as resource :refer [with-resources]]
     [arctype.service.protocol :refer :all])
@@ -13,20 +14,25 @@
 (def Config
   {:db-spec {S/Keyword S/Any}})
 
+(defn- xform-db-spec
+  [spec]
+  (cond-> spec
+    (some? (:connection-uri spec)) (-> (assoc :jdbc-url (:connection-uri spec))
+                                       (dissoc :connection-uri))))
+
 (defn- connect
   "Open a jdbc connection pool"
   [config]
-  ; http://clojure.github.io/java.jdbc/#clojure.java.jdbc/get-connection
-  (let [db-spec (:db-spec config)]
-    (log/debug {:message "Opening JDBC connection"
+  (let [db-spec (xform-db-spec (:db-spec config))]
+    (log/debug {:message "Opening Hikari connection pool to SQLite"
                 :db-spec db-spec})
-    (jdbc/get-connection db-spec)))
+    (hikari-cp/make-datasource db-spec)))
 
 (defn- disconnect
   "Release a jdbc connection pool"
   [db]
-  (log/debug {:message "Closing JDBC connection"})
-  (.close db)
+  (log/debug {:message "Closing Hikari connection pool to SQLite"})
+  (hikari-cp/close-datasource db)
   nil)
 
 (defn health-check!
@@ -49,21 +55,21 @@
   [results]
   (second (ffirst results)))
 
-(defrecord SQLiteClient [config connection]
+(defrecord SQLiteClient [config datasource]
   PLifecycle
   (start [this]
     (log/info "Starting SQLite client")
     (-> this
-        (assoc :connection (connect config))))
+        (assoc :datasource (connect config))))
 
   (stop [this]
     (log/info "Stopping SQLite client")
     (-> this
-        (update :connection disconnect)))
+        (update :datasource disconnect)))
 
   PJdbcConnection
   (conn [this] 
-    {:connection connection}))
+    {:datasource datasource}))
 
 (S/defn create
   [resource-name config :- Config]
