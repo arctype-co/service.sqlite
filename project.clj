f2c33881497a641eac3f3504fcac2773cb3266b6
Ryan Sundberg <ryan.sundberg@gmail.com>
2020-10-05 21:45:14 -0700

    include jdbc dpenedency

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :dependencies 
   [[org.clojure/clojure "1.10.0"]
    [org.clojure/core.async "0.4.500"]
+   [org.clojure/java.jdbc "0.7.11"]
    [arctype/service "1.0.4"]
    [org.xerial/sqlite-jdbc "3.28.0"]])
